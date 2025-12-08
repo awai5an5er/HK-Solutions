dd20d130316a4e44e258b62438c2315df410e552
Junaidasif975 <junaidasif250@gmail.com>
2025-12-08 17:32:41 +0500

    make footer

---
+++ app/resources/footer.ts
@@ -0,0 +1,52 @@
+interface servicesType{
+    name:string;
+    link:string;
+}
+export const services:servicesType[]=[
+    {
+        name:"Solar PV Panels",
+        link:"/"
+    },
+    {
+        name:"Air Source Heat Pumps",
+        link:"/"
+    },
+    {
+        name:"Home insulation",
+        link:"/"
+    },
+    {
+        name:"Battery Storage",
+        link:"/"
+    },
+    {
+        name:"EV Charging",
+        link:"/"
+    }
+];
+interface quicklinksType{
+    name:string;
+    link:string;
+}
+export const quiklinks:quicklinksType[]=[
+    {
+        name:"About Us",
+        link:"/"
+    },
+    {
+        name:"Government Grants",
+        link:"/"
+    },
+    {
+        name:"Case Studies",
+        link:"/"
+    },
+    {
+        name:"Contact Us",
+        link:"/"
+    },
+    {
+        name:"FAqs",
+        link:"/"
+    }
+];
